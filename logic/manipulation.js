c52fd21ea908bc4e1c215432739b89ef66f08fb3
Rodrigo <kauarodrigo1193@gmail.com>
2024-03-27 00:59:10 -0300

    Fazendo mais anotações sobre Manipulação de vetores e matrizes

---
+++ logic/manipulation.js
@@ -0,0 +1,25 @@
+// Criando matrizes e manipulando vetores
+
+const nomesPokemon = ["Mewtwo", "Charmeleon", "Raykow", "Gengar", "Gastly"];
+
+let timePokemon = [
+  ["Mewtwo", "Masculino", "Level 1"],
+  ["Row-Row", "Feminino", "Level 5"],
+];
+
+console.log(timePokemon[1][2]);
+
+/* O exemplo acima se trata de uma matriz bidimensional onde existe 2 objetos
+
+o primeiro começa pelo time do mewtwo
+o outro começa com o Row-Row
+
+Ao acessar o item 1 no console eu passo pro array/vetor debaixo e após chamalo eu insiro outra posição se tratando de um elemento que o acima é o level 4
+
+*/
+
+/*_____________________*/
+
+// Métodos - São ações que podemos fazer com os nossos vetores/matrizes
+
+// As propriedades são caracteristicas são coisas que me falam fatos sobre sua matriz como o .length
