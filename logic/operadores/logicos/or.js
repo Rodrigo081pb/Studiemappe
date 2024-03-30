6fc59de19abdcbbe2b491adff1f8ae50a4f4cb8b
Rodrigo <kauarodrigo1193@gmail.com>
2024-03-30 12:02:57 -0300

    Adicionando conteudo da parte lógica com operadores lógicos

---
+++ logic/operadores/logicos/or.js
@@ -0,0 +1,21 @@
+// Operador Ou No javascript - or ||
+
+/*
+
+ Operador lógico (ou) no javascript é ||
+
+Ele pergunta ao usuário se ele quer isso ou aquilo exemplo :
+
+if ((IdadeUsuario1 => IdadeUsuario2) || (IdadeUsuario1 <= IdadeUsuario2)) {
+console.log("Eles possuem idades diferentes")
+} else {
+console.log("Eles possuem idades iguais)
+}
+
+*/
+
+let item = "Roupão";
+let neve = "sim";
+const sair = neve === "sim" || item === "Roupão";
+
+console.log(sair);
